6cb008f2d49726c7216a5f8cf880ce6c377c4778
PWarwickDev <pwarwick272@gmail.com>
2025-02-01 19:54:02 -0500

    First commit

---
+++ src/DMG-CPU/registers.c
@@ -0,0 +1,41 @@
+// Most registers are able to be accessed as either one 16bit or 2 8-bit registers.
+#include <stdbool.h>
+
+struct registers 
+{
+    // Accumulator and flags
+    unsigned char A;
+    unsigned char F;
+
+    // BC
+    unsigned char B;
+    unsigned char C;
+
+    // DE
+    unsigned char D;
+    unsigned char E;
+
+    // HL
+    unsigned char H;
+    unsigned char L;
+};
+
+// Flag register is within lower 8bit of the AF register
+// Ex. 0xZNHC0000
+struct flags 
+{
+    // Zero flag (1 if current op results in zero)
+    unsigned char Z;
+
+    // Subtraction flag (1 if subtraction performed)
+    unsigned char N;
+
+    // Half carry flag (1 if carry occurred from lower nibble of last op)
+    // * lower nibble means lowest 4 bits * 
+    unsigned char H;
+
+    // Carry flag (1 if carry occurred in last op, or if A is smaller val in CP instr)
+    unsigned char C;
+
+    bool HALT;
+};
